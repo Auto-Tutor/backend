b30fe05d0be9e5916ff513e72a5706cbdae09c22
Исупов Станислав <isupov.s@tot.technology>
2019-04-09 17:25:20 +0300

    Добавлена простая html страница

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from flask import request
+import json
 from src.core.selection import Selection
+
+from src.htmlforms.testform import TestForm
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/erwerwerw/', methods=['GET', 'POST'])
 def hello_world():
     return Selection().get_text_v1(request.get_json(force=True))
 
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world_2():
+    a = request.args.get('answer1')
+    b = request.args.get('answer2')
+    c = request.args.get('answer3')
+
+    if a is None or b is None or c is None:
+        return TestForm().get_text_v1()
+
+    return 'Молодец, прошел тест'
 
 if __name__ == '__main__':
     app.run()
