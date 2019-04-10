68a6d07a094540116feb9cd4afab67dec94671a1
Natalia <isupovand@yandex-team.ru>
2019-04-10 21:28:41 +0300

    12. добавлена страница с логином

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask
-from flask import request
+from flask import \
+    (Flask,
+     request,
+     g,
+     session)
 import json
 from src.core.selection import Selection
 
 from src.htmlforms.testform import TestForm
 
 app = Flask(__name__)
+
+# Мы не поняли, что это, но ладно
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/erwerwerw/', methods=['GET', 'POST'])
@@ -15,12 +21,19 @@ def hello_world():
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world_2():
+    if session.get('login', None) is None:
+        login = request.args.get('login')
+        if login is None:
+            return TestForm().login()
+
+        session['login'] = login
+
     a = request.args.get('answer1')
     b = request.args.get('answer2')
     c = request.args.get('answer3')
 
     if a is None or b is None or c is None:
-        return TestForm().get_text_v1()
+        return TestForm().test()
 
     return 'Молодец, прошел тест'
 
--- src/htmlforms/testform.py
+++ src/htmlforms/testform.py
@@ -3,47 +3,14 @@ import json
 
 class TestForm():
     def get_text_v1(self) -> json:
-        return """
- <!DOCTYPE HTML>
- <html>
- <head>
- 	<meta charset="utf-8">
- 	<title>Тег FORM</title>
- </head>
- <body>
+        return
 
- 	<form >
- 		<p><b>Однажды в поликлинику пришел больной.
- 			– Что у вас болит? – спросил врач.
- 			– У меня болит живот, – ответил молодой человек.
- 			– Что вы ели вчера?
- 			– Зеленые яблоки.
- 			– Хорошо. Я дам вам лекарство для глаз, – сказал врач больному.
- 			– Почему для глаз? Ведь у меня болит живот? – удивился молодой человек.
- 		– Я дам вам лекарство для глаз, чтобы вы лучше видели, что вы едите, – сказал врач.</b></p>
+    def login(self):
+        with open('src/htmlforms/login.html', "r") as f:
+            name_form = f.read()
+        return name_form
 
- 		<p><b>Почему пациент пришел к врачу?</b></p>
- 		<p><input type="radio" name="answer1" value="a1">у него болели глаза;<Br>
- 			<input type="radio" name="answer1" value="a2">у него проблемы с животом;<Br>
- 			<input type="radio" name="answer1" value="a3">у него проблемы с аппетитом - он ест только зеленые яблоки
- 		</p>
-
-
- 		<p><b>Почему пациент плохо себя чувствует?</b></p>
- 		<p><input type="radio" name="answer2" value="a1">ему нужно лекарство;<Br>
- 			<input type="radio" name="answer2" value="a2">это - плановый визит;<Br>
- 			<input type="radio" name="answer2" value="a3">вчера он ел зеленые яблоки.
- 		</p>
-
-
- 		<p><b>Почему доктор выписал пациенту лекарство для глаз?</b></p>
- 		<p><input type="radio" name="answer3" value="a1">у пациента болели глаза;<Br>
- 			<input type="radio" name="answer3" value="a2">чтобы он не ел зеленые яблоки;<Br>
- 			<input type="radio" name="answer3" value="a3"> лекарство для глаз может лечить и больной живот.</p>
- 			<p>
- 				<input type="submit"></p>
- 			</form>
-
- 		</body>
- 		</html>s
-        """
+    def test(self):
+        with open("src/htmlforms/test.html", "r") as f:
+            test_form = f.read()
+        return test_form
